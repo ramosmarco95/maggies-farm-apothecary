b2e4018d87cd0d491c9d9e0e1772a06bc4c8f038
marco_ramos <ramosmarco95@gmail.com>
2025-05-04 14:28:42 -0600

    changes3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename='/maggies-farm-apothecary'>
+  <StrictMode>
+    <BrowserRouter basename='/maggies-farm-apothecary/'>
       <App />
     </BrowserRouter>,
-  </React.StrictMode>,
+  </StrictMode>,
 )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  base: '/maggies-farm-apothecary/',
   server: {
     host: '0.0.0.0',
     port: 5173
