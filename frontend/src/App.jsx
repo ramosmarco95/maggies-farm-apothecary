39bc56707c22fa5c2b85428634c9f881e7d6aa95
Marco Ramos <ramosmarco95@gmail.com>
2025-05-07 10:35:37 -0600

    Added contact page and .gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,12 @@ import "./index.css";
 // pages
 import ProductsPage from "./pages/ProductsPage/ProductsPage.jsx"
 import HomePage from './pages/HomePage/HomePage.jsx';
+import ContactPage from './pages/ContactPage/ContactPage.jsx';
 
 // components
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
+
 
 function App() {
  
@@ -21,7 +23,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="/services" element={<h1>Services</h1>} />
         <Route path="/products" element={<ProductsPage />} />
       </Routes>
